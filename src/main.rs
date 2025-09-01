1e09c71418d6c7bf641e79fee59c0b83d43420b3
AleQuesada2012 <ale11199@estudiantec.cr>
2025-09-01 09:14:33 -0600

    Add a json containing syscalls and data
    from a tutorial on YouTube, a syscall.json is created to aid in the task of intercepting and stopping syscalls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
     println!("Hello, world from Rust!");
+    my_func()
+}
+
+fn my_func() {
+    println!("Hello from a function!");
+    let x = 5;
+    let name = "Alejandro";
+    println!("Hello, {}!", name);
+    println!("your age is: {}", x)
 }
