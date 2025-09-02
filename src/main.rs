f89353e545e9223d1006e281f7f44c1ebe9bec2a
AleQuesada2012 <ale11199@estudiantec.cr>
2025-09-02 09:00:54 -0600

    Add tracing code to main.rs
    credits given to github.com/fasterthanlime for the code at main.rs at the moment
    
    I followed his tutorial on implementing a tracing program that I will base my further changes on.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,60 @@
-fn main() {
-    println!("Hello, world from Rust!");
-    my_func()
-}
+/*
+Taken verbatim from https://github.com/fasterthanlime/rue/
+For testing purposes due to similarity and their tutorial about strace
+Will be modifying in the near future
+ */
 
-fn my_func() {
-    println!("Hello from a function!");
-    let x = 5;
-    let name = "Alejandro";
-    println!("Hello, {}!", name);
-    println!("your age is: {}", x)
+use std::{collections::HashMap, os::unix::process::CommandExt, process::Command};
+
+use nix::{
+    sys::{ptrace, wait::waitpid},
+    unistd::Pid,
+};
+use owo_colors::OwoColorize;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let json: serde_json::Value = serde_json::from_str(include_str!("../syscall.json"))?;
+    let syscall_table: HashMap<u64, String> = json["aaData"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|item| {
+            (
+                item[0].as_u64().unwrap(),
+                item[1].as_str().unwrap().to_owned(),
+            )
+        })
+        .collect();
+
+    let mut command = Command::new("cat");
+    command.arg("/etc/hosts");
+    unsafe {
+        command.pre_exec(|| {
+            use nix::sys::ptrace::traceme;
+            traceme().map_err(|e| e.into())
+        });
+    }
+
+    let child = command.spawn()?;
+    let child_pid = Pid::from_raw(child.id() as _);
+    let res = waitpid(child_pid, None)?;
+    eprintln!("first wait: {:?}", res.yellow());
+
+    let mut is_sys_exit = false;
+    loop {
+        ptrace::syscall(child_pid, None)?;
+        _ = waitpid(child_pid, None)?;
+        if is_sys_exit {
+            let regs = ptrace::getregs(child_pid)?;
+            eprintln!(
+                "{}({:x}, {:x}, {:x}, ...) = {:x}",
+                syscall_table[&regs.orig_rax].green(),
+                regs.rdi.blue(),
+                regs.rsi.blue(),
+                regs.rdx.blue(),
+                regs.rax.yellow(),
+            );
+        }
+        is_sys_exit = !is_sys_exit;
+    }
 }
