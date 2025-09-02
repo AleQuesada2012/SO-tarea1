58b447aea5c02070b4903b68dec7e21401fce3d7
AleQuesada2012 <ale11199@estudiantec.cr>
2025-09-01 22:56:11 -0600

    remove cross and try calling another program

---
+++ target/debug/deps/rastreador-d8f1121af35a7bf3.d
@@ -0,0 +1,5 @@
+/home/zekken/OpSys/SO-tarea1/target/debug/deps/rastreador-d8f1121af35a7bf3: src/main.rs
+
+/home/zekken/OpSys/SO-tarea1/target/debug/deps/rastreador-d8f1121af35a7bf3.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rastreador.d
@@ -0,0 +1,1 @@
+/home/zekken/OpSys/SO-tarea1/target/debug/rastreador: /home/zekken/OpSys/SO-tarea1/src/main.rs
